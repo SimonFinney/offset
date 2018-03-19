45025c7c0a63301fa9078d81e2661ecce9c552c5
Simon Finney <simonfin@ie.ibm.com>
2018-03-19 01:01:14 +0000

    chore(minify): minify response

--- index.js
+++ index.js
@@ -3,21 +3,28 @@
 const compression = require('compression');
 const dotenv = require('dotenv');
 const express = require('express');
+const expressBeautify = require('express-beautify');
 const nunjucks = require('nunjucks');
 
 dotenv.load();
 
-const router = require('./src/router');
-const socket = require('./src/socket');
-
 const app = express();
 
 const serverDirectory = app.get('env') === 'development' ? '.tmp' : 'dist';
-const staticAssets = express.static(`${__dirname}/${serverDirectory}/`);
 
-app.use(staticAssets);
+app.use(
+  expressBeautify.minify({
+    htmlMinifier: {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+    },
+  })
+);
 
-app.use('/', router);
+app.use(express.static(`${__dirname}/${serverDirectory}/`));
 
 app.use(compression());
 
@@ -30,6 +37,9 @@ app.host = app.set('host', process.env.HOST || 'http://localhost');
 app.port = app.set('port', process.env.PORT || 8080);
 
 const port = app.get('port');
+
+const socket = require('./src/socket');
+require('./src/router')(app);
 
 const server = app.listen(port, () => {
   console.log(`${app.get('host')}:${port}`);
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 // TODO: Document
 
-const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 const yaml = require('js-yaml');
@@ -12,49 +11,37 @@ const util = require('./util');
 const content = yaml.safeLoad(fs.readFileSync('content/content.yml', 'utf-8'));
 
 const name = require('../package.json').name;
-const router = express.Router();
 const upload = multer();
 
 function getRoute(request) {
   return request.url.replace('/', '');
 }
 
-router.get('/', (request, response) => {
-  const route = getRoute(request);
+module.exports = server => {
+  server.get('/', (request, response) => {
+    const route = getRoute(request);
 
-  content.questions = util.shuffle(content.questions);
+    content.questions = util.shuffle(content.questions);
 
-  response.render('views/index.nunjucks', {
-    content,
-    name,
-    route,
+    response.render('views/index.nunjucks', { content, name, route });
   });
-});
 
-router.get('/receive', (request, response) => {
-  const route = getRoute(request);
+  server.get('/receive', (request, response) => {
+    const route = getRoute(request);
 
-  database.get(data => {
-    const images = data && Object.keys(data).map(id => data[id]);
+    database.get(data => {
+      const images = data && Object.keys(data).map(id => data[id]);
 
-    response.render('views/receive.nunjucks', {
-      images,
-      name,
-      route,
+      response.render('views/receive.nunjucks', { images, name, route });
     });
   });
-});
 
-router.post('/submit', upload.single('file'), (request, response) => {
-  const modifier = request.body.modifier;
-  const src = img.convert(request.file);
+  server.post('/submit', upload.single('file'), (request, response) => {
+    const modifier = request.body.modifier;
+    const src = img.convert(request.file);
 
-  const data = {
-    modifier,
-    src,
-  };
+    const data = { modifier, src };
 
-  database.create(data, () => response.redirect('/'));
-});
-
-module.exports = router;
+    database.create(data, () => response.redirect('/'));
+  });
+};
